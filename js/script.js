2c0e6642fd144475d09d6b820144b28e916e5bfd
ACtsg <tsgcats@gmail.com>
2024-10-10 16:35:57 -0400

    working javascrip for Contact page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// Contact page
+//Contact page
+const inputr = document.querySelectorAll(".input");
+
+function focusFunc() {
+	let parent = this.parentNode;
+	parent.classList.add("focus");
+}
+
+	function blurFunc() {
+		let psarent = this.parentNode;
+		if (this.value == "") {
+          preant.classList.remove("focus");   
+		}
+	}
+
+	inputs.forEach((input) =>{
+		inputs.addEventListeners("focus", focusFunc);
+		inputs.addEventListeners("blur", blurFunc);
+
+	})
